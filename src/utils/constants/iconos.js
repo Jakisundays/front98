ddff1f85ea8059bf27fa929345c07d7527cb9405
Jakisundays <jakisundays@gmail.com>
2023-02-01 20:29:57 -0300

    front end

--- src/utils/constants/iconos.js
+++ src/utils/constants/iconos.js
@@ -3,12 +3,12 @@ import { loginSwitch, registerSwitch } from "../../store/slices/windowsSlice";
 export const iconos = [
   {
     title: "Sign up",
-    icon: require("/Users/jacobdominguez/Documents/codiyapa/MERN/Mern Auth/client/src/assets/compu.png"),
+    icon: require("../../assets/compu.png"),
     funcion: () => registerSwitch(),
   },
   {
     title: "Login",
-    icon: require("/Users/jacobdominguez/Documents/codiyapa/MERN/Mern Auth/client/src/assets/key.ico"),
+    icon: require("../../assets/key.ico"),
     funcion: () => loginSwitch(),
   },
 ];
