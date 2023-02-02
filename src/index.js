4b799c2627ec043c5c86fdc56f866c1db49a450e
Jakisundays <jakisundays@gmail.com>
2023-02-01 22:56:34 -0300

    router fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "98.css";
 import { Provider } from "react-redux";
 import store from "./store/store";
@@ -11,9 +11,9 @@ if (process.env.NODE_ENV === "production") disableReactDevTools();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 );
